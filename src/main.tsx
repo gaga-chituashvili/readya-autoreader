b623f4743565d58b22f380edc8371908177ffc8a
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-04-10 13:15:29 +0400

    feat: add Vercel Speed Insights to React app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import App from "@/App.tsx";
 import "@/i18n";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class">
         <App />
+        <SpeedInsights />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
