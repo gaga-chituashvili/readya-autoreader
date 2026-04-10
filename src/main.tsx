ed96ec0a866c1ed945acdf650ef58dc41570478e
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-04-10 13:27:57 +0400

    feat: add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "@/index.css";
 import App from "@/App.tsx";
 import "@/i18n";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
@@ -15,8 +16,9 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class">
         <App />
-        <SpeedInsights />
       </ThemeProvider>
+      <SpeedInsights />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 );
